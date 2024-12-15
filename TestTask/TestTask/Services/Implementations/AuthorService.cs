5b3c726683805893a980dfbaae04ac2d01eac8d4
KotVMeshke <dimon2003mail.ru37@gmail.com>
2024-12-15 12:53:06 +0300

    Completed test task

---
+++ TestTask/TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class AuthorService(ApplicationDbContext context) : IAuthorService
+{
+    public async Task<Author> GetAuthor()
+    {
+        return await context.Authors
+                .Include(a => a.Books)
+                .Where(a => a.Books.Any())
+                .OrderByDescending(a => a.Books
+                    .OrderByDescending(b => b.Title.Length)
+                    .Max(b => b.Title.Length))
+                .ThenBy(a => a.Id)
+                .Select(a => new Author()
+                {
+                    Books = null,
+                    Id = a.Id,
+                    Name = a.Name,
+                    Surname = a.Surname
+                })
+                .FirstOrDefaultAsync();
+    }
+
+    public async Task<List<Author>> GetAuthors()
+    {
+        return await context.Authors
+                .Include(a => a.Books)
+                .Where(a => a.Books.Any(b => b.PublishDate.Year > 2015) && a.Books
+                    .Count(b => b.PublishDate.Year > 2015) % 2 == 0)
+                .Select(a => new Author()
+                {
+                    Books = null,
+                    Id = a.Id,
+                    Name = a.Name,
+                    Surname = a.Surname
+                })
+                .ToListAsync();
+    }
+}
---
+++ TestTask/TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class BookService(ApplicationDbContext context) : IBookService
+{
+    public async Task<Book> GetBook()
+    {
+        return await context.Books
+                    .OrderByDescending(b => b.QuantityPublished * b.Price)
+                    .FirstOrDefaultAsync();
+    }
+
+    public async Task<List<Book>> GetBooks()
+    {
+        return await context.Books
+                    .Where(b => EF.Functions.Like(b.Title,"%Red%")
+                        && b.PublishDate > new DateTime(2012,5,25))
+                    .ToListAsync();
+    }
+}
